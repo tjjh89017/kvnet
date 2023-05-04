3389061921ca89a822bfbb879f2bb9a74fb6f53d
Date Huang <tjjh89017@hotmail.com>
2023-05-04 21:16:45 +0800

    test
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 make -C .. undeploy IMG="tjjh89017/kvnet:v0.0.1"
 make -C .. docker-build IMG="tjjh89017/kvnet:v0.0.1"
 kind load docker-image "tjjh89017/kvnet:v0.0.1" -n kvnet-test 
-make -C .. undeploy IMG="tjjh89017/kvnet:v0.0.1"
+make -C .. deploy IMG="tjjh89017/kvnet:v0.0.1"
